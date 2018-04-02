2d33b75fa8c906b2100ad22a788a8e2b696c5fa9
youminxiaodao <1126519721@qq.com>
2018-04-02 18:05:31 +0800

    Signed-off-by: youminxiaodao <1126519721@qq.com>

--- src/com/wlb/T24/in/sea/test/Test.java
+++ src/com/wlb/T24/in/sea/test/Test.java
@@ -1,7 +1,17 @@
 package com.wlb.T24.in.sea.test;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Test {
 	public String test(String T){
+		
 		return T;
+	}
+	public static void main(String[] args){
+		Date date = new Date();
+		System.out.println(date);
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyymmdd");
+		System.out.println(simpleDateFormat.format(date));
 	}
 }
